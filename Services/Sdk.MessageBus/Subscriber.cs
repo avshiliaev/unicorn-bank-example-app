c1a79287c84bf9c6dfd14ecf3cd13472055cfb07
Artem Shiliaev <>
2020-10-15 14:18:03 +0200

    init PubSub

---
+++ Services/Sdk.MessageBus/Subscriber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Sdk.MessageBus
+{
+    public class Subscriber
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory {HostName = "localhost"};
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("hello",
+                    false,
+                    false,
+                    false,
+                    null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+                channel.BasicConsume("hello",
+                    true,
+                    consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
