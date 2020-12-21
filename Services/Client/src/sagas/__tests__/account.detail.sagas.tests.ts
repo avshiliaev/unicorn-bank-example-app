ed9456d209ec1c76c39964f4f44f141f072dc448
Artem <Shiliaev>
2020-12-21 09:02:52 +0100

    clean up

--- Services/Client/src/sagas/__tests__/account.detail.sagas.tests.ts
+++ Services/Client/src/sagas/__tests__/account.detail.sagas.tests.ts
@@ -1,13 +1,11 @@
 import {getAccountDetailSaga} from "../account.detail.sagas";
 import {getAccount} from "../../reducers/account.reducer";
 import {AccountAction} from "../../interfaces/account.interface";
-import {call, take, put} from "redux-saga/effects";
+import {call, put, take} from "redux-saga/effects";
 import createWebSocketConnection from "../../web.socket";
 import {createSocketChannel} from "../api";
 import {ActionTypes} from "../../constants";
 
-class StreamResponse {
-}
 
 //TODO https://redux-saga.js.org/docs/advanced/Testing.html
 describe('getAccountDetailSaga tests', () => {
@@ -22,7 +20,6 @@ describe('getAccountDetailSaga tests', () => {
         expect(saga.next().value).toStrictEqual(call(createWebSocketConnection, path));
         expect(saga.next().value).toStrictEqual(call(createSocketChannel, undefined));
 
-        const streamResponse: StreamResponse = {}
         expect(saga.next().value).toStrictEqual(take());
 
         const actionError: AccountAction = {
