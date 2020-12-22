0919efc5a1cf5ead5be5fccdfcb0c725670faeb9
Artem <a.v.shiliaev@gmail.com>
2020-12-22 09:38:12 +0100

    clean up the store craetion logic

--- Services/Client/src/index.tsx
+++ Services/Client/src/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import store, {reachHistory} from './store';
 import {Provider} from 'react-redux';
 import './index.css';
 import {LocationProvider} from '@reach/router';
 import App from "./app";
+import rootSaga from "./sagas";
+import {createReduxHistoryContext, reachify} from "redux-first-history";
+import {createBrowserHistory} from "history";
+import configureStore from "./store";
+
+
+const {createReduxHistory, routerMiddleware} = createReduxHistoryContext({
+    history: createBrowserHistory(),
+});
+const store = configureStore(routerMiddleware);
+const reachHistory = reachify(createReduxHistory(store));
+
+store.runSaga(rootSaga)
 
 ReactDOM.render(
     <Provider store={store}>
