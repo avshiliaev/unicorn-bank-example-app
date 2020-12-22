b6d561246969ba124913f6ae2c73195466529a1d
Artem <a.v.shiliaev@gmail.com>
2020-12-22 22:20:56 +0100

    auth0 user interface

--- Services/Client/src/interfaces/auth.interface.ts
+++ Services/Client/src/interfaces/auth.interface.ts
@@ -1,3 +1,13 @@
+export interface Auth0User {
+    email: string
+    email_verified: boolean
+    name: string
+    nickname: string
+    picture: string
+    sub: string
+    updated_at: string
+}
+
 // General
 export interface AuthInterface {
     isLoggedIn: boolean
