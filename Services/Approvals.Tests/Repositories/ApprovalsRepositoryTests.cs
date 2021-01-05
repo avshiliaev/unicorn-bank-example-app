5936e80d2149052ec52c01a3239d11ccc598b36a
Artem <Shiliaev>
2021-01-05 15:58:05 +0100

    clean up

--- Services/Approvals.Tests/Repositories/ApprovalsRepositoryTests.cs
+++ Services/Approvals.Tests/Repositories/ApprovalsRepositoryTests.cs
@@ -11,157 +11,156 @@ namespace Approvals.Tests.Repositories
 {
     public class ApprovalsRepositoryTests
     {
-        
-    public class TestContext : ApprovalsContext
-    {
-        protected DbContextOptions<ApprovalsContext> ContextOptions { get; }
-        
-        public TestContext(DbContextOptions<ApprovalsContext> options)
-            : base(options)
+        public class TestContext : ApprovalsContext
         {
-            ContextOptions = options;
+            public TestContext(DbContextOptions<ApprovalsContext> options)
+                : base(options)
+            {
+                ContextOptions = options;
+            }
+
+            protected DbContextOptions<ApprovalsContext> ContextOptions { get; }
+
+            public DbSet<ApprovalEntity> Entities { get; set; }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<ApprovalEntity>().ToTable("Entities");
+                base.OnModelCreating(modelBuilder);
+            }
         }
 
-        public DbSet<ApprovalEntity> Entities { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        public class AbstractRepositoryTests : TestContext
         {
-            modelBuilder.Entity<ApprovalEntity>().ToTable("Entities");
-            base.OnModelCreating(modelBuilder);
-        }
-    }
-
-    public class AbstractRepositoryTests : TestContext
-    {
-        public AbstractRepositoryTests()
-            : base(
-                new DbContextOptionsBuilder<ApprovalsContext>()
-                    .UseInMemoryDatabase("InMemoryDbForTesting")
-                    .Options
+            public AbstractRepositoryTests()
+                : base(
+                    new DbContextOptionsBuilder<ApprovalsContext>()
+                        .UseInMemoryDatabase("InMemoryDbForTesting")
+                        .Options
                 )
-        {
-        }
-        
-        [Fact]
-        public async void Should_AddAsync_Valid()
-        {
-            await using var context = new TestContext(ContextOptions);
-            var repository = new ApprovalsRepository(
-                new Mock<ILogger<AbstractRepository<ApprovalsContext, ApprovalEntity>>>().Object,
-                context
-            );
-            var newEntity = new ApprovalEntity();
-            var result = await repository.AddAsync(newEntity);
-            Assert.NotNull(result);
-        }
-        
-        [Fact]
-        public async void Should_ListAllAsync_Valid()
-        {
-            await using var context = new TestContext(ContextOptions);
-            var repository = new ApprovalsRepository(
-                new Mock<ILogger<AbstractRepository<ApprovalsContext, ApprovalEntity>>>().Object,
-                context
-            );
-            var newEntity = new ApprovalEntity();
-            var _ = await repository.AddAsync(newEntity);
-            var result = await repository.ListAllAsync();
-            Assert.NotNull(result);
-            Assert.NotEmpty(result);
-        }
-        
-        [Fact]
-        public async void Should_GetByIdAsync_Valid()
-        {
-            await using var context = new TestContext(ContextOptions);
-            var repository = new ApprovalsRepository(
-                new Mock<ILogger<AbstractRepository<ApprovalsContext, ApprovalEntity>>>().Object,
-                context
-            );
-            var newEntity = new ApprovalEntity();
-            var newEntitySaved = await repository.AddAsync(newEntity);
-            var result = await repository.GetByIdAsync(newEntitySaved.Id);
-            Assert.NotNull(result);
-        }
-        
-        [Fact]
-        public async void Should_GetOneByParameterAsync_Valid()
-        {
-            await using var context = new TestContext(ContextOptions);
-            var repository = new ApprovalsRepository(
-                new Mock<ILogger<AbstractRepository<ApprovalsContext, ApprovalEntity>>>().Object,
-                context
-            );
-            var newEntity = new ApprovalEntity{Version = 99};
-            var _ = await repository.AddAsync(newEntity);
-            
-            var result = await repository.GetOneByParameterAsync(
-                (e) =>  e.Version == 99
-                );
-            Assert.NotNull(result);
-        }
-        
-        [Fact]
-        public async void Should_GetManyByParameterAsync_Valid()
-        {
-            await using var context = new TestContext(ContextOptions);
-            var repository = new ApprovalsRepository(
-                new Mock<ILogger<AbstractRepository<ApprovalsContext, ApprovalEntity>>>().Object,
-                context
-            );
-            var newEntity = new ApprovalEntity{Version = 10};
-            var _ = await repository.AddAsync(newEntity);
-            
-            var result = await repository.GetManyByParameterAsync(
-                (e) =>  e.Version == 10
-                );
-            Assert.Single(result);
-        }
-        
-        [Fact]
-        public async void Should_DeleteAsync_Valid()
-        {
-            await using var context = new TestContext(ContextOptions);
-            var repository = new ApprovalsRepository(
-                new Mock<ILogger<AbstractRepository<ApprovalsContext, ApprovalEntity>>>().Object,
-                context
-            );
-            var newEntity = new ApprovalEntity();
-            var newEntitySaved = await repository.AddAsync(newEntity);
-            var result = await repository.DeleteAsync(newEntitySaved.Id);
-            Assert.NotNull(result);
-        }
-        
-        [Fact]
-        public async void Should_UpdateActivelyAsync_Valid()
-        {
-            await using var context = new TestContext(ContextOptions);
-            var repository = new ApprovalsRepository(
-                new Mock<ILogger<AbstractRepository<ApprovalsContext, ApprovalEntity>>>().Object,
-                context
-            );
-            var newEntity = new ApprovalEntity();
-            var newEntitySaved = await repository.AddAsync(newEntity);
+            {
+            }
 
-            var result = await repository.UpdateActivelyAsync(newEntitySaved);
-            Assert.NotNull(result);
-        }
-        
-        [Fact]
-        public async void Should_UpdatePassivelyAsync_Valid()
-        {
-            await using var context = new TestContext(ContextOptions);
-            var repository = new ApprovalsRepository(
-                new Mock<ILogger<AbstractRepository<ApprovalsContext, ApprovalEntity>>>().Object,
-                context
-            );
-            var newEntity = new ApprovalEntity();
-            var newEntitySaved = await repository.AddAsync(newEntity);
-            newEntitySaved.Version++;
+            [Fact]
+            public async void Should_AddAsync_Valid()
+            {
+                await using var context = new TestContext(ContextOptions);
+                var repository = new ApprovalsRepository(
+                    new Mock<ILogger<AbstractRepository<ApprovalsContext, ApprovalEntity>>>().Object,
+                    context
+                );
+                var newEntity = new ApprovalEntity();
+                var result = await repository.AddAsync(newEntity);
+                Assert.NotNull(result);
+            }
 
-            var result = await repository.UpdatePassivelyAsync(newEntitySaved);
-            Assert.NotNull(result);
+            [Fact]
+            public async void Should_ListAllAsync_Valid()
+            {
+                await using var context = new TestContext(ContextOptions);
+                var repository = new ApprovalsRepository(
+                    new Mock<ILogger<AbstractRepository<ApprovalsContext, ApprovalEntity>>>().Object,
+                    context
+                );
+                var newEntity = new ApprovalEntity();
+                var _ = await repository.AddAsync(newEntity);
+                var result = await repository.ListAllAsync();
+                Assert.NotNull(result);
+                Assert.NotEmpty(result);
+            }
+
+            [Fact]
+            public async void Should_GetByIdAsync_Valid()
+            {
+                await using var context = new TestContext(ContextOptions);
+                var repository = new ApprovalsRepository(
+                    new Mock<ILogger<AbstractRepository<ApprovalsContext, ApprovalEntity>>>().Object,
+                    context
+                );
+                var newEntity = new ApprovalEntity();
+                var newEntitySaved = await repository.AddAsync(newEntity);
+                var result = await repository.GetByIdAsync(newEntitySaved.Id);
+                Assert.NotNull(result);
+            }
+
+            [Fact]
+            public async void Should_GetOneByParameterAsync_Valid()
+            {
+                await using var context = new TestContext(ContextOptions);
+                var repository = new ApprovalsRepository(
+                    new Mock<ILogger<AbstractRepository<ApprovalsContext, ApprovalEntity>>>().Object,
+                    context
+                );
+                var newEntity = new ApprovalEntity {Version = 99};
+                var _ = await repository.AddAsync(newEntity);
+
+                var result = await repository.GetOneByParameterAsync(
+                    e => e.Version == 99
+                );
+                Assert.NotNull(result);
+            }
+
+            [Fact]
+            public async void Should_GetManyByParameterAsync_Valid()
+            {
+                await using var context = new TestContext(ContextOptions);
+                var repository = new ApprovalsRepository(
+                    new Mock<ILogger<AbstractRepository<ApprovalsContext, ApprovalEntity>>>().Object,
+                    context
+                );
+                var newEntity = new ApprovalEntity {Version = 10};
+                var _ = await repository.AddAsync(newEntity);
+
+                var result = await repository.GetManyByParameterAsync(
+                    e => e.Version == 10
+                );
+                Assert.Single(result);
+            }
+
+            [Fact]
+            public async void Should_DeleteAsync_Valid()
+            {
+                await using var context = new TestContext(ContextOptions);
+                var repository = new ApprovalsRepository(
+                    new Mock<ILogger<AbstractRepository<ApprovalsContext, ApprovalEntity>>>().Object,
+                    context
+                );
+                var newEntity = new ApprovalEntity();
+                var newEntitySaved = await repository.AddAsync(newEntity);
+                var result = await repository.DeleteAsync(newEntitySaved.Id);
+                Assert.NotNull(result);
+            }
+
+            [Fact]
+            public async void Should_UpdateActivelyAsync_Valid()
+            {
+                await using var context = new TestContext(ContextOptions);
+                var repository = new ApprovalsRepository(
+                    new Mock<ILogger<AbstractRepository<ApprovalsContext, ApprovalEntity>>>().Object,
+                    context
+                );
+                var newEntity = new ApprovalEntity();
+                var newEntitySaved = await repository.AddAsync(newEntity);
+
+                var result = await repository.UpdateActivelyAsync(newEntitySaved);
+                Assert.NotNull(result);
+            }
+
+            [Fact]
+            public async void Should_UpdatePassivelyAsync_Valid()
+            {
+                await using var context = new TestContext(ContextOptions);
+                var repository = new ApprovalsRepository(
+                    new Mock<ILogger<AbstractRepository<ApprovalsContext, ApprovalEntity>>>().Object,
+                    context
+                );
+                var newEntity = new ApprovalEntity();
+                var newEntitySaved = await repository.AddAsync(newEntity);
+                newEntitySaved.Version++;
+
+                var result = await repository.UpdatePassivelyAsync(newEntitySaved);
+                Assert.NotNull(result);
+            }
         }
-    }
     }
 }
