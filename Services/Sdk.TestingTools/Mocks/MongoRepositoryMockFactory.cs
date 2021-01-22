ff4c8a4ceb3fa4bc985eeb7cd8cfd672062ec945
Artem Shiliaev <a.v.shiliaev@gmail.com>
2021-01-22 12:52:16 +0100

    fixed tests

--- Services/Sdk.TestingTools/Mocks/MongoRepositoryMockFactory.cs
+++ Services/Sdk.TestingTools/Mocks/MongoRepositoryMockFactory.cs
@@ -60,7 +60,9 @@ namespace Sdk.Tests.Mocks
                 .Setup(a => a.Get(It.IsAny<string>()))
                 .Returns((string id) => _entities.FirstOrDefault(e => e.Id == id));
             repository
-                .Setup(a => a.SubscribeToChangesStreamMany(It.IsAny<BsonDocument[]>()))
+                .Setup(a => a.SubscribeToChangesStreamMany(
+                    It.IsAny<PipelineDefinition<ChangeStreamDocument<TEntity>, ChangeStreamDocument<TEntity>>>())
+                )
                 .Returns((BsonDocument pipeline) => new MongoChangeStreamMock<TEntity>(_entities));
             repository
                 .Setup(a => a.Create(It.IsAny<TEntity>()))
